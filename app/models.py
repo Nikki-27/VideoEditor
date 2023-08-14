7334ee0195e7fbedabdbdebe61e5df718dd472a3
Nikki-27 <87594658+Nikki-27@users.noreply.github.com>
2023-08-14 23:49:08 +0530

    Delete app directory

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from databases import db
-
-class Subtitle(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.Float, nullable=False)
-    text = db.Column(db.String(200), nullable=False)
-    video_filename = db.Column(db.String(100), nullable=False)
-    subtitled_video_path = db.Column(db.String(100))
