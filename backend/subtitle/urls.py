b679a60fb15c4452076562c81d4c9d2d7ca1f613
Nikki-27 <87594658+Nikki-27@users.noreply.github.com>
2023-08-14 23:48:51 +0530

    Delete backend directory

--- backend/subtitle/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TaskListCreateView,VideoUploadView
-
-
-urlpatterns = [
-    path('api/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('api/upload/', VideoUploadView.as_view(), name='video-upload'),
-]
